09ca3cefd38fbf68efb0bccd9dc3d3727b13c688
arief1103 <73701136+arief110301@users.noreply.github.com>
2023-08-09 14:31:39 +0700

    Add files via upload

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'User1',
+            'email' => 'user1@email.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
